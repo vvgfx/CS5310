a56134303a166842cb4e9cdcb614f6a47679719f
Vishnu Vardan <vishnuvardan662000@gmail.com>
2025-05-09 10:06:06 -0400

    assignment 5 starter code

---
+++ assignment5/Scenegraphs.cpp
@@ -0,0 +1,29 @@
+//! [code]
+
+#include <glad/glad.h>
+#include <string>
+#include "View.h"
+#include "Model.h"
+#include "Controller.h"
+#include <iostream>
+
+int main(int argc,char *argv[]) {
+    std::vector<std::string> args(argv, argv + argc);
+    // cout<<"reached here: "<<argc<<endl;
+    Model model;
+    View view;
+    if(argc == 1)
+    {
+        Controller controller(model,view, "");
+        controller.run();
+    }
+    else
+    {
+        Controller controller(model,view, args[1]);
+        controller.run();
+    }
+
+
+}
+
+//! [code]
